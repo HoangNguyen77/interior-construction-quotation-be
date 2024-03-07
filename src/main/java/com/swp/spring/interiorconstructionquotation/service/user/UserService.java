5a687d1c0ef2331938f7d1d6bc3747bdcaef06a0
Lưu Lưu <130716543+ethavais@users.noreply.github.com>
2024-03-07 13:03:56 +0700

    Update UserService.java

--- src/main/java/com/swp/spring/interiorconstructionquotation/service/user/UserService.java
+++ src/main/java/com/swp/spring/interiorconstructionquotation/service/user/UserService.java
@@ -123,16 +123,23 @@ public class UserService implements IUserService {
     @Override
     @Transactional
     public ResponseEntity<?> updateUserEnabledStatus(int userId, boolean enabled) {
-        try{
+        try {
             User user = iUserRepository.findByUserId(userId);
             user.setEnabled(enabled);
             iUserRepository.saveAndFlush(user);
 
-            return  ResponseEntity.ok().body("Change user status successfully");
-        } catch (Exception e){
+            return ResponseEntity.ok().body("Change user status successfully");
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return ResponseEntity.badRequest().
                     body("Change failed due to an error: \" + e.getMessage()");
         }
+    }
+    public void sendBanEmail(String email) {
+        String subject = "Tài khoản của bạn đã bị ban tại VivaDecod";
+        String text = "Tài khoản của bạn đã bị ban tại VivaDecod, vui lòng liên hệ qua email" +
+                ": vivadecor88@gmail.com để biết thêm chi tiết! ";
+
+        iEmailService.sendEmail("vivadecor88@gmail.com", email, subject, text);
     }
 }
